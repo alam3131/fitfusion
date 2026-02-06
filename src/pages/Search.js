10df37e6edadc63a70b49fd725127d2e88add344
alam3131 <lamanthony678@gmail.com>
2026-02-05 23:08:49 -0600

    Search and filters working with new API

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,41 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Button, FormControl, MenuItem, Select, Stack, TextField, Typography } from '@mui/material';
-import { exerciseOptions, exerciseUrl, fetchData } from '../utils/fetchData';
-import { Link } from 'react-router-dom'; 
-import Pagination from '@mui/material/Pagination';
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { exerciseOptions, exerciseUrl, fetchData } from "../utils/fetchData";
+import { Link } from "react-router-dom";
+import Pagination from "@mui/material/Pagination";
 
-export const muscleGroups = await fetchData(`${exerciseUrl}/targetList`, exerciseOptions);
-const levelGroups =['beginner','intermediate','advanced'];
+export const muscleGroups = await fetchData(
+  `${exerciseUrl}/targetList`,
+  exerciseOptions,
+);
+const levelGroups = ["beginner", "intermediate", "advanced"];
 
 // Function to calculate points based on level
 const getPointsForLevel = (level) => {
   switch (level) {
-    case 'beginner':
+    case "beginner":
       return 100;
-    case 'intermediate':
+    case "intermediate":
       return 200;
-    case 'advanced':
+    case "advanced":
       return 300;
     default:
       return 0;
   }
 }; //******************************************* */
 
-const SearchExcercises = ({ earnPoints, points, setWorkoutsToCalender, workoutsToCalender }) => {
-  const [search, setSearch] = useState('');
-  const [excercises,setExcercises] = useState([]);
-  const [selectedMuscle, setSelectedMuscle] = useState('');
-  const [selectedLevel, setLevel] = useState('');
+const SearchExcercises = ({
+  earnPoints,
+  points,
+  setWorkoutsToCalender,
+  workoutsToCalender,
+}) => {
+  const [search, setSearch] = useState("");
+  const [excercises, setExcercises] = useState([]);
+  const [selectedMuscle, setSelectedMuscle] = useState("");
+  const [selectedLevel, setLevel] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [exercisesPerPage] = useState(6);
 
   const handleLevelAndMuscleFilter = async () => {
     if (selectedMuscle && selectedLevel) {
-      const exercisesData = await fetchData(exerciseUrl, exerciseOptions);
-      const filteredExercises = exercisesData.filter(
-        item => item.Muscles.toLowerCase().includes(selectedMuscle.toLowerCase()) &&
-                item.Intensity_Level.toLowerCase().includes(selectedLevel.toLowerCase())
+      let offset = 0; // Offset to track where last request left off
+      let exercisesData = []; // Array of exercises retrieved from request
+
+      // Make two requests to get a total of 20 exercises
+      // Appends responses to exercisesData array
+      for (let i = 0; i < 2; i++) {
+        const response = await fetchData(
+          `${exerciseUrl}/target/${selectedMuscle}?offset=${offset}`,
+          exerciseOptions,
+        );
+        exercisesData = exercisesData.concat(response);
+        offset += 10;
+      }
+
+      // Filters the exercises to only include ones with specified difficulty level
+      const filteredExercises = exercisesData.filter((item) =>
+        item.difficulty.toLowerCase().includes(selectedLevel.toLowerCase()),
       );
+      console.log("The filtered Exercises:", filteredExercises);
       setExcercises(filteredExercises);
     } else if (selectedMuscle) {
       handleMuscleGroupClick(selectedMuscle);
@@ -45,153 +76,229 @@ const SearchExcercises = ({ earnPoints, points, setWorkoutsToCalender, workoutsT
   };
 
   const handleLevelGroupClick = async (Level) => {
-    setLevel(Level); 
+    setLevel(Level);
     const exercisesData = await fetchData(exerciseUrl, exerciseOptions);
-    const filteredByLevel = exercisesData.filter(item => item.Intensity_Level.toLowerCase().includes(Level.toLowerCase()));
+    // console.log(exercisesData[0].difficulty);
+    const filteredByLevel = exercisesData.filter((item) =>
+      item.difficulty.toLowerCase().includes(Level.toLowerCase()),
+    );
     setExcercises(filteredByLevel);
   };
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = excercises.slice(indexOfFirstExercise, indexOfLastExercise);
+  const currentExercises = excercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise,
+  );
 
   const paginate = (event, value) => {
     setCurrentPage(value);
 
-    window.scrollTo({ top: 1800, behavior: 'smooth' });
+    window.scrollTo({ top: 1800, behavior: "smooth" });
   };
 
   const handleMuscleGroupClick = async (muscle) => {
-    setSelectedMuscle(muscle); 
+    setSelectedMuscle(muscle);
     const exercisesData = await fetchData(exerciseUrl, exerciseOptions);
-    const filteredExercises = exercisesData.filter(item => item.Muscles.toLowerCase().includes(muscle.toLowerCase()));
+    const filteredExercises = exercisesData.filter((item) =>
+      item.Muscles.toLowerCase().includes(muscle.toLowerCase()),
+    );
     setExcercises(filteredExercises);
   };
 
   const handleSearch = async () => {
     if (search) {
-      const exercisesData = await fetchData(exerciseUrl, exerciseOptions);
-      const searchedExercises = exercisesData.filter(
-        item => item.WorkOut.toLowerCase().includes(search)||item.Muscles.toLowerCase().includes(search)|| item.Intensity_Level.toLowerCase().includes(search) 
-        || (item['Long Explanation'].toLowerCase().includes(search)),);
-      setSearch('');
-      console.log(exercisesData);
-      setExcercises(searchedExercises);
+      // Searches based on names that have the keyword
+      const exercisesData = await fetchData(`${exerciseUrl}/name/${search}`, exerciseOptions); 
+      // TODO: Refactor this code so filters work together with search bar
+      // const searchedExercises = exercisesData.filter(
+      //   (item) =>
+      //     item.name.toLowerCase().includes(search) ||
+      //     [item.target, ...(item.secondaryMuscles ?? [])].join(", ").toLowerCase().includes(search) ||
+      //     item.difficulty.toLowerCase().includes(search)
+      // );
+      setSearch("");
+      setExcercises(exercisesData);
     }
-  }
+  };
 
   const handleExcerciseClick = (exercise) => {
     // earnPoints(Number(getPointsForLevel(exercise.Intensity_Level)));
     // alert("Muscle Groups" + JSON.stringify(currentExercises));
   }; //****************************************** */
-  
+
   useEffect(() => {
     handleLevelAndMuscleFilter();
   }, [selectedMuscle, selectedLevel]);
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }, [currentPage]);
-  
+
   return (
-    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px" spacing={4}>
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+    <Stack
+      alignItems="center"
+      mt="37px"
+      justifyContent="center"
+      p="20px"
+      spacing={4}
+    >
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="49px"
+        textAlign="center"
+      >
         Awesome Exercises For <br /> You
       </Typography>
       <Stack direction="row" spacing={2}>
         <TextField
           height="76px"
-          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '550px', xs: '250px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-          value = {search}
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
+            width: { lg: "550px", xs: "250px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#128731', color: '#fff', textTransform: 'none', height: '56px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#128731",
+            color: "#fff",
+            textTransform: "none",
+            height: "56px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={handleSearch}
+        >
           Search
         </Button>
       </Stack>
       <Stack direction="row" spacing={2}>
-      <FormControl sx={{ width: '200px',bgcolor: '#128731', borderRadius: '4px', fontSize: { lg: '20px', xs: '14px' } }}> 
-        <Select className="search-btn"
-          sx={{color: '#fff', fontSize: { lg: '20px', xs: '14px' }, height: '56px'}}
-          value={selectedMuscle}
-          onChange={(e) => setSelectedMuscle(e.target.value)}
-          displayEmpty
-           // Customize scrollbar width bgcolor: '#128731' sx={{ color: '#FFFFFF' }
+        <FormControl
+          sx={{
+            width: "200px",
+            bgcolor: "#128731",
+            borderRadius: "4px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
         >
-        <MenuItem value="" disabled >
-          Muscle Group
-        </MenuItem>
-        {muscleGroups.map((muscle, index) => (
-          <MenuItem key={index} value={muscle}>{muscle.charAt(0).toUpperCase() + muscle.slice(1)}</MenuItem>
-        ))}
-        </Select>
-      </FormControl>
-      <FormControl sx={{ width: '250px',bgcolor: '#128731', borderRadius: '4px', fontSize: { lg: '20px', xs: '14px' }}}> 
-        <Select className="search-btn"
-          sx={{color: '#fff', fontSize: { lg: '20px', xs: '14px' }, height: '56px'}}
-          value={selectedLevel}
-          onChange={(e) => setLevel(e.target.value)}
-          displayEmpty
-           // Customize scrollbar width bgcolor: '#128731' sx={{ color: '#FFFFFF' }
+          <Select
+            className="search-btn"
+            sx={{
+              color: "#fff",
+              fontSize: { lg: "20px", xs: "14px" },
+              height: "56px",
+            }}
+            value={selectedMuscle}
+            onChange={(e) => setSelectedMuscle(e.target.value)}
+            displayEmpty
+            // Customize scrollbar width bgcolor: '#128731' sx={{ color: '#FFFFFF' }
+          >
+            <MenuItem value="" disabled>
+              Muscle Group
+            </MenuItem>
+            {muscleGroups.map((muscle, index) => (
+              <MenuItem key={index} value={muscle}>
+                {muscle.charAt(0).toUpperCase() + muscle.slice(1)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl
+          sx={{
+            width: "250px",
+            bgcolor: "#128731",
+            borderRadius: "4px",
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
         >
-        <MenuItem value="" disabled >
-          Experience Level
-        </MenuItem>
-        {levelGroups.map((level, index) => (
-          <MenuItem key={index} value={level}>{level.charAt(0).toUpperCase() + level.slice(1)}</MenuItem>
-        ))}
-        </Select>
-      </FormControl>
-        
+          <Select
+            className="search-btn"
+            sx={{
+              color: "#fff",
+              fontSize: { lg: "20px", xs: "14px" },
+              height: "56px",
+            }}
+            value={selectedLevel}
+            onChange={(e) => setLevel(e.target.value)}
+            displayEmpty
+            // Customize scrollbar width bgcolor: '#128731' sx={{ color: '#FFFFFF' }
+          >
+            <MenuItem value="" disabled>
+              Experience Level
+            </MenuItem>
+            {levelGroups.map((level, index) => (
+              <MenuItem key={index} value={level}>
+                {level.charAt(0).toUpperCase() + level.slice(1)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </Stack>
       <Stack spacing={2} flexWrap="wrap" justifyContent="center">
-  {currentExercises.length > 0 ? (
-    currentExercises.map((exercise, index) => (
-      <Button 
-        key={index} 
-        onClick={() => handleExcerciseClick(exercise)} 
-        variant="contained" 
-        sx={{ 
-          bgcolor: '#128731',
-          color: '#fff', // White text color
-          borderRadius: 4, 
-          textAlign: 'center',
-          mt: 25,
-          mx: 'auto',
-          display: 'block',
-          width: '300px',
-          textTransform: 'none',
-          '&:hover': {
-            bgcolor: 'transparent', // Transparent background color on hover
-            borderColor: 'red', // Red border color on hover
-            '& > .MuiBox-root': {
-              color: 'red', // Change text color of the box to red on hover
-              borderColor: 'red', // Change border color of the box to red on hover
-            }
-          }
-        }}
-      >
-        <Box p={2} borderRadius={4} sx={{ color: '#fff' }}>
-          <Typography variant="h6">{exercise.WorkOut}</Typography>
-          <Typography variant="body1">Muscles: {exercise.Muscles}</Typography>
-          <Typography variant="body2">Level: {exercise.Intensity_Level}</Typography>
-          <Typography variant="body2">Points: {getPointsForLevel(exercise.Intensity_Level)}</Typography>
-          <Link to={`/search/${exercise.WorkOut}`} state={exercise}>
-            <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff'}}>View Details</Button>
-          </Link>
-        </Box>
-      </Button>
-    ))
-  ) :  (
-    <Typography >No results found...</Typography>
-  )}
-</Stack>
+        {currentExercises.length > 0 ? (
+          currentExercises.map((exercise, index) => (
+            <Button
+              key={index}
+              onClick={() => handleExcerciseClick(exercise)}
+              variant="contained"
+              sx={{
+                bgcolor: "#128731",
+                color: "#fff", // White text color
+                borderRadius: 4,
+                textAlign: "center",
+                mt: 25,
+                mx: "auto",
+                display: "block",
+                width: "300px",
+                textTransform: "none",
+                "&:hover": {
+                  bgcolor: "transparent", // Transparent background color on hover
+                  borderColor: "red", // Red border color on hover
+                  "& > .MuiBox-root": {
+                    color: "red", // Change text color of the box to red on hover
+                    borderColor: "red", // Change border color of the box to red on hover
+                  },
+                },
+              }}
+            >
+              <Box p={2} borderRadius={4} sx={{ color: "#fff" }}>
+                <Typography variant="h6">{exercise.name}</Typography>
+                <Typography variant="body1">
+                  Muscles: {[exercise.target, ...(exercise.secondaryMuscles ?? [])].join(", ")}
+                </Typography>
+                <Typography variant="body2">
+                  Level: {exercise.difficulty}
+                </Typography>
+                <Typography variant="body2">
+                  Points: {getPointsForLevel(exercise.difficulty)}
+                </Typography>
+                <Link to={`/search/${exercise.name}`} state={exercise}>
+                  <Button
+                    className="search-btn"
+                    sx={{ bgcolor: "#FF2625", color: "#fff" }}
+                  >
+                    View Details
+                  </Button>
+                </Link>
+              </Box>
+            </Button>
+          ))
+        ) : (
+          <Typography>No results found...</Typography>
+        )}
+      </Stack>
 
-      <Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems="center">
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
         {excercises.length > 9 && (
-        <Pagination
+          <Pagination
             color="standard"
             shape="rounded"
             defaultPage={1}
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,35 +1,36 @@
-export const exerciseUrl = 'https://exercisedb.p.rapidapi.com/exercises';
-export const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+export const exerciseUrl = "https://exercisedb.p.rapidapi.com/exercises";
+export const youtubeSearchUrl =
+  "https://youtube-search-and-download.p.rapidapi.com";
 
 export const exerciseOptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
-		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+  },
 };
 
 export const youtubeOptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
-		'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+    "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
+  },
 };
 
 export const fetchData = async (url, options) => {
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json(); // TODO: May have to change this to .json(). Was in .text()
-      console.log(result);
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json(); // TODO: May have to change this to .json(). Was in .text()
+    // console.log(result);
 
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 
-    // const response = await fetch(url,options);
-    // const data = await response.json()
+  // const response = await fetch(url,options);
+  // const data = await response.json()
 
-    // return data;
-}
+  // return data;
+};
